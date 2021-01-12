a3e2a505ec46e9b1bcd332a305183bcdf41f742a
Justin Whittaker <just.whittaker@gmail.com>
2021-01-12 11:58:25 +0000

    feat(ClickIT):customized admin

--- products/models.py
+++ products/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+        class Meta:
+            verbose_name_plural = 'Categories'
+
+        name = models.CharField(max_length=254)
+        friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -24,4 +28,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-        
