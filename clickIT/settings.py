a6c7f38ceb0e2644ef6b571c4aa80061892c62c4
Justin Whittaker <just.whittaker@gmail.com>
2021-01-13 12:13:07 +0000

    feat(clickit):updated sizes

--- clickIT/settings.py
+++ clickIT/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'boutique_ado.urls'
+ROOT_URLCONF = 'clickIT.urls'
 
 TEMPLATES = [
     {
@@ -95,7 +95,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'boutique_ado.wsgi.application'
+WSGI_APPLICATION = 'clickIT.wsgi.application'
 
 
 # Database
